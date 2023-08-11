e9d674a6eccc78aa4af216849770077682adfc7d
archerax <10404171+archerax@users.noreply.github.com>
2023-08-11 18:43:35 +0100

    select team backlog on spin

---
+++ src/utils/delay.ts
@@ -0,0 +1,5 @@
+export function delay(milliseconds: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+        setTimeout(() => { resolve(undefined); }, milliseconds);
+    });
+}
