f90d5007403e412ffc9fce980ed7b94a93b50bfe
Anthony <10404171+archerax@users.noreply.github.com>
2023-08-14 16:27:06 +0100

    select user on spin

--- src/utils/formatName.ts
+++ src/utils/formatName.ts
@@ -2,16 +2,16 @@ export function formatName(name: string|null): string {
     if (name) {
         name = name.replace(/\s*<.+/, "");
 
-        const reverseMatch = name.match(/^(\w+), (\w+).*$/);
+        const reverseMatch = name.match(/^(\w+), (.+).*$/);
 
         if (reverseMatch) {
-            return `${reverseMatch[2]}`;
+            return `${reverseMatch[2]} ${reverseMatch[1]}`;
         }
 
-        const match = name.match(/^(\w+) (\w+).*$/);
+        const match = name.match(/^(\w+) (.+)$/);
 
         if (match) {
-            return `${match[1]}`;
+            return `${match[1]} ${match[2]}`;
         }
     }
     return name ?? "";
