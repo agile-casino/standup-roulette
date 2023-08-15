159b21779ac623a6b06a763843ef47304ef3e5d2
archerax <10404171+archerax@users.noreply.github.com>
2023-08-15 05:48:38 +0100

    tidy code

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,5 +1,7 @@
 export function delay(milliseconds: number): Promise<void> {
-    return new Promise<void>((resolve) => {
-        setTimeout(() => { resolve(undefined); }, milliseconds);
-    });
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve(undefined);
+    }, milliseconds);
+  });
 }
