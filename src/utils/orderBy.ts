4832ec7a924f38e148c5846029ba4fcda76bc5e1
archerax <10404171+archerax@users.noreply.github.com>
2023-08-15 05:45:38 +0100

    tidy code

--- src/utils/orderBy.ts
+++ src/utils/orderBy.ts
@@ -1,48 +1,41 @@
-
 export type OrderByFn<TObject> = (a: TObject, b: TObject) => number;
 
 export function ascending<TValue>(valA: TValue, valB: TValue): number {
+  if (valA > valB) {
+    return 1;
+  }
+  else if (valB > valA) {
+    return -1;
+  }
 
-    if ( valA > valB ) {
-        return 1;
-    }
-    else if ( valB > valA ) {
-        return -1;
-    }
-
-    return 0;
+  return 0;
 }
 
 export function orderBy<TObject, TValue>(value: (x: TObject) => TValue): OrderByFn<TObject> {
+  return function (a: TObject, b: TObject) {
+    const valA = value(a);
+    const valB = value(b);
 
-    return function (a: TObject, b: TObject) {
-
-        const valA = value(a);
-        const valB = value(b);
-
-        return ascending(valA, valB);
-    };
+    return ascending(valA, valB);
+  };
 }
 
 export function descending<TValue>(valA: TValue, valB: TValue): number {
+  if (valA > valB) {
+    return -1;
+  }
+  else if (valB > valA) {
+    return 1;
+  }
 
-    if (valA > valB) {
-        return -1;
-    }
-    else if (valB > valA) {
-        return 1;
-    }
-
-    return 0;
+  return 0;
 }
 
 export function orderByDescending<TObject, TValue>(value: (x: TObject) => TValue): OrderByFn<TObject> {
+  return function (a: TObject, b: TObject) {
+    const valA = value(a);
+    const valB = value(b);
 
-    return function (a: TObject, b: TObject) {
-
-        const valA = value(a);
-        const valB = value(b);
-
-        return descending(valA, valB);
-    };
+    return descending(valA, valB);
+  };
 }
