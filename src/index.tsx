c361a66f86a0a52f540e2ce42a524f03d2af5f9f
archerax <10404171+archerax@users.noreply.github.com>
2023-08-15 05:06:42 +0100

    tidy code

--- src/index.tsx
+++ src/index.tsx
@@ -12,16 +12,20 @@ let url = window.location.href;
 const urlChangeEvent = new Event("urlChange");
 
 const observer = new MutationObserver(() => {
-    if (window.location.href !== url) {
-        window.dispatchEvent(urlChangeEvent);
-        url = window.location.href;
+  if (window.location.href !== url) {
+    window.dispatchEvent(urlChangeEvent);
+    url = window.location.href;
+  }
+  if (!isInDocument(container)) {
+    const searchHeader = document.querySelector(".expandable-search-header");
+    if (searchHeader) {
+      searchHeader.parentElement?.insertBefore(container, searchHeader);
     }
-    if (!isInDocument(container)) {
-        const searchHeader = document.querySelector(".expandable-search-header");
-        if (searchHeader) {
-            searchHeader.parentElement?.insertBefore(container, searchHeader);
-        }
-    }
+  }
 });
 
-observer.observe(document.body, { subtree: true, characterData: true, childList: true });
+observer.observe(document.body, {
+  subtree: true,
+  characterData: true,
+  childList: true
+});
