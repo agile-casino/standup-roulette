968d797e2d3ea6e6b2131fb66d34a0de1d0a5b0d
archerax <10404171+archerax@users.noreply.github.com>
2023-08-18 19:49:18 +0100

    switch to using user ids

--- src/store/roulette/rouletteSlice.ts
+++ src/store/roulette/rouletteSlice.ts
@@ -1,10 +1,13 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import type { RootState } from "../index";
+import { RootState } from "../index";
 import { getColourScheme } from "../../utils/colourScheme";
 import { orderBy } from "../../utils/orderBy";
 import { deepCopy } from "../../utils/deepCopy";
 import { User } from "./User";
 import { getLocalStorage, setLocalStorage } from "../../utils/localStorage";
+import { v4 as uuidv4 } from "uuid";
+
+const uuid = uuidv4 as () => string;
 
 export interface RouletteState {
   allUsers: User[];
@@ -14,13 +17,30 @@ export interface RouletteState {
   winningName: string | null;
 }
 
-const initialState: RouletteState = {
-  allUsers: getLocalStorage<User[]>("standup-roulette:allUsers", []),
-  remainingUsers: getLocalStorage<User[]>("standup-roulette:remainingUsers", []),
-  spinning: false,
-  winningIndex: getLocalStorage<number | null>("standup-roulette:winningIndex", null),
-  winningName: getLocalStorage<string | null>("standup-roulette:winningName", null)
-};
+function importLegacyState(): RouletteState {
+  const state: RouletteState = {
+    allUsers: getLocalStorage<User[]>("standup-roulette:allUsers", []),
+    remainingUsers: getLocalStorage<User[]>("standup-roulette:remainingUsers", []),
+    spinning: false,
+    winningIndex: getLocalStorage<number | null>("standup-roulette:winningIndex", null),
+    winningName: getLocalStorage<string | null>("standup-roulette:winningName", null)
+  };
+
+  for (const user of state.allUsers) {
+    if (!user.id) {
+      user.id = uuid();
+    }
+  }
+
+  for (const user of state.remainingUsers) {
+    const matchingUser = state.allUsers.find(u => u.name === user.name);
+    user.id = matchingUser?.id ?? uuid();
+  }
+
+  return state;
+}
+
+const initialState: RouletteState = importLegacyState();
 
 function assignColours(users: User[]): void {
   const selectedUsers = users.filter((x) => x.checked);
@@ -35,29 +55,38 @@ export const rouletteSlice = createSlice({
   initialState: initialState,
   reducers: {
     addUser: (state, action: PayloadAction<{ name: string }>) => {
-      state.allUsers = [...state.allUsers, { name: action.payload.name }].sort(orderBy((x) => x.name));
+      state.allUsers = [...state.allUsers, { id: uuid(), name: action.payload.name }].sort(orderBy((x) => x.name));
       assignColours(state.allUsers);
       setLocalStorage("standup-roulette:allUsers", state.allUsers);
     },
-    removeUser: (state, action: PayloadAction<{ index: number }>) => {
-      state.allUsers = state.allUsers.filter((_, index) => index !== action.payload.index);
+    removeUser: (state, action: PayloadAction<{ id: string }>) => {
+      state.allUsers = state.allUsers.filter(u => u.id !== action.payload.id);
       assignColours(state.allUsers);
       setLocalStorage("standup-roulette:allUsers", state.allUsers);
     },
-    setUserName: (state, action: PayloadAction<{ index: number; newUserName: string }>) => {
-      state.allUsers[action.payload.index].name = action.payload.newUserName;
-      state.allUsers = state.allUsers.sort(orderBy((x) => x.name));
-      assignColours(state.allUsers);
-      setLocalStorage("standup-roulette:allUsers", state.allUsers);
+    setUserName: (state, action: PayloadAction<{ id: string; newUserName: string }>) => {
+      const user = state.allUsers.find(u => u.id === action.payload.id);
+      if (user) {
+        user.name = action.payload.newUserName;
+        state.allUsers = state.allUsers.sort(orderBy((x) => x.name));
+        assignColours(state.allUsers);
+        setLocalStorage("standup-roulette:allUsers", state.allUsers);
+      }
     },
-    setUserTeam: (state, action: PayloadAction<{ index: number; newTeamName: string }>) => {
-      state.allUsers[action.payload.index].team = action.payload.newTeamName;
-      setLocalStorage("standup-roulette:allUsers", state.allUsers);
+    setUserTeam: (state, action: PayloadAction<{ id: string; newTeamName: string }>) => {
+      const user = state.allUsers.find(u => u.id === action.payload.id);
+      if (user) {
+        user.team = action.payload.newTeamName;
+        setLocalStorage("standup-roulette:allUsers", state.allUsers);
+      }
     },
-    toggleUser: (state, action: PayloadAction<{ index: number }>) => {
-      state.allUsers[action.payload.index].checked = !state.allUsers[action.payload.index].checked;
-      assignColours(state.allUsers);
-      setLocalStorage("standup-roulette:allUsers", state.allUsers);
+    toggleUser: (state, action: PayloadAction<{ id: string }>) => {
+      const user = state.allUsers.find(u => u.id === action.payload.id);
+      if (user) {
+        user.checked = !user.checked;
+        assignColours(state.allUsers);
+        setLocalStorage("standup-roulette:allUsers", state.allUsers);
+      }
     },
     prepareSpin: (state) => {
       if (state.winningIndex !== null) {
