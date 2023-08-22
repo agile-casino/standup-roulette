f8e94ab2692f5daeb577d4680022ba7403014e35
archerax <10404171+archerax@users.noreply.github.com>
2023-08-22 14:56:54 +0100

    linting

--- src/store/persist.ts
+++ src/store/persist.ts
@@ -7,13 +7,13 @@ export interface PersistSettings<TState> {
 }
 
 export function persist<TState, TAction extends Action>(reducer: Reducer<TState, TAction>, settings: PersistSettings<TState>): Reducer<TState, TAction> {
-  const excludeStateKeys = settings.exclude?.map(x => x.toString());
-  return function(state: TState | undefined, action: TAction) {
+  const excludeStateKeys = settings.exclude?.map((x) => x.toString());
+  return function (state: TState | undefined, action: TAction) {
     if (state === undefined) {
-      state = getLocalStorage<TState|undefined>(settings.key, undefined);
+      state = getLocalStorage<TState | undefined>(settings.key, undefined);
     }
     const newState = reducer(state, action);
     setLocalStorage(settings.key, newState, excludeStateKeys);
     return newState;
-  }
+  };
 }
