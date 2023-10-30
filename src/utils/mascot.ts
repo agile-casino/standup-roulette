b466d33381b6f7219141909a5531dc71c12f0a7d
Adam <adam.edmunds@weatherford.com>
2023-10-30 09:34:18 +0000

    Added totodile for my name

--- src/utils/mascot.ts
+++ src/utils/mascot.ts
@@ -1,7 +1,16 @@
 import { crc32 } from "@aws-crypto/crc32";
 
+const getHardCodedValues = (name: string) => {
+  switch (name.toLowerCase()) {
+    case "adam e":
+      return 158;
+    default:
+      return crc32((new TextEncoder).encode(name)) % 749;
+  }
+}
+
 export function getMascot(name: string): { uri: string } {
-  const mascotIndex = crc32((new TextEncoder).encode(name)) % 749;
+  const mascotIndex = getHardCodedValues(name);
   return {
     uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mascotIndex}.png`
   }
