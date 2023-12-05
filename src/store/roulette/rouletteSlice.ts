6d5572034bbfe7c7b0504917a43590ad3dbe1cf3
archerax <10404171+archerax@users.noreply.github.com>
2023-12-05 18:24:32 +0000

    allow team switching

--- src/store/roulette/rouletteSlice.ts
+++ src/store/roulette/rouletteSlice.ts
@@ -1,32 +1,27 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, current, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../index";
 import { getColourScheme } from "../../utils/colourScheme";
 import { orderBy } from "../../utils/orderBy";
 import { deepCopy } from "../../utils/deepCopy";
-import { User } from "./User";
+import { RouletteState } from "./state";
+import { RouletteUser } from "./state";
 import { v4 as uuidv4 } from "uuid";
 
 const uuid = uuidv4 as () => string;
 
-interface RouletteState {
-  allUsers: User[];
-  remainingUsers: User[];
-  spinning: boolean;
-  winningId: string | null;
-  winningName: string | null;
-  seed: number;
-}
-
 const initialState: RouletteState = {
-  allUsers: [],
-  remainingUsers: [],
-  spinning: false,
-  winningId: null,
-  winningName: null,
-  seed: 0
+  currentGame: 0,
+  games: [{
+    allUsers: [],
+    remainingUsers: [],
+    spinning: false,
+    winningId: null,
+    winningName: null,
+    seed: 0
+  }]
 };
 
-function assignColours(users: User[]): void {
+function assignColours(users: RouletteUser[]): void {
   const selectedUsers = users.filter((x) => x.checked);
   const colours = getColourScheme(selectedUsers.length);
   for (let i = 0; i < colours.length; i++) {
@@ -38,73 +33,108 @@ export const rouletteSlice = createSlice({
   name: "roulette",
   initialState: initialState,
   reducers: {
+    prevGame: (state) => {
+      if (state.games[state.currentGame].spinning) {
+        return;
+      }
+      const index = Math.max(state.currentGame - 1, 0);
+      if (!state.games[index]) {
+        state.games[index] = {
+          allUsers: [],
+          remainingUsers: [],
+          spinning: false,
+          winningId: null,
+          winningName: null,
+          seed: 0
+        };
+      }
+      state.currentGame = index;
+    },
+    nextGame: (state) => {
+      if (state.games[state.currentGame].spinning) {
+        return;
+      }
+      const index = Math.min(state.currentGame + 1, 2);
+      if (!state.games[index]) {
+        state.games[index] = {
+          allUsers: [],
+          remainingUsers: [],
+          spinning: false,
+          winningId: null,
+          winningName: null,
+          seed: 0
+        };
+      }
+      state.currentGame = index;
+    },
     addUser: (state, action: PayloadAction<{ name: string }>) => {
-      state.allUsers = [...state.allUsers, { id: uuid(), name: action.payload.name }].sort(orderBy((x) => x.name));
-      assignColours(state.allUsers);
+      state.games[state.currentGame].allUsers = [...state.games[state.currentGame].allUsers, { id: uuid(), name: action.payload.name }].sort(orderBy((x) => x.name));
+      assignColours(state.games[state.currentGame].allUsers);
     },
     removeUser: (state, action: PayloadAction<{ id: string }>) => {
-      state.allUsers = state.allUsers.filter((u) => u.id !== action.payload.id);
-      assignColours(state.allUsers);
+      state.games[state.currentGame].allUsers = state.games[state.currentGame].allUsers.filter((u) => u.id !== action.payload.id);
+      assignColours(state.games[state.currentGame].allUsers);
     },
     setUserName: (state, action: PayloadAction<{ id: string; newUserName: string }>) => {
-      const user = state.allUsers.find((u) => u.id === action.payload.id);
+      const user = state.games[state.currentGame].allUsers.find((u) => u.id === action.payload.id);
       if (user) {
         user.name = action.payload.newUserName;
-        state.allUsers = state.allUsers.sort(orderBy((x) => x.name));
-        assignColours(state.allUsers);
+        state.games[state.currentGame].allUsers = state.games[state.currentGame].allUsers.sort(orderBy((x) => x.name));
+        assignColours(state.games[state.currentGame].allUsers);
       }
     },
     setUserTeam: (state, action: PayloadAction<{ id: string; newTeamName: string }>) => {
-      const user = state.allUsers.find((u) => u.id === action.payload.id);
+      const user = state.games[state.currentGame].allUsers.find((u) => u.id === action.payload.id);
       if (user) {
         user.team = action.payload.newTeamName;
       }
     },
     toggleUser: (state, action: PayloadAction<{ id: string }>) => {
-      const user = state.allUsers.find((u) => u.id === action.payload.id);
+      const user = state.games[state.currentGame].allUsers.find((u) => u.id === action.payload.id);
       if (user) {
         user.checked = !user.checked;
-        assignColours(state.allUsers);
+        assignColours(state.games[state.currentGame].allUsers);
       }
     },
     prepareSpin: (state, action: PayloadAction<{ random: number }>) => {
-      if (state.winningId !== null) {
-        const newRemainingUsers = state.remainingUsers.filter((u) => u.id !== state.winningId);
-        state.remainingUsers = newRemainingUsers;
+      if (state.games[state.currentGame].winningId !== null) {
+        const newRemainingUsers = state.games[state.currentGame].remainingUsers.filter((u) => u.id !== state.games[state.currentGame].winningId);
+        state.games[state.currentGame].remainingUsers = newRemainingUsers;
       }
-      if (state.remainingUsers.length > 0) {
-        const winningIndex = Math.floor(action.payload.random * state.remainingUsers.length);
-        state.winningId = state.remainingUsers[winningIndex].id;
+      if (state.games[state.currentGame].remainingUsers.length > 0) {
+        const winningIndex = Math.floor(action.payload.random * state.games[state.currentGame].remainingUsers.length);
+        state.games[state.currentGame].winningId = state.games[state.currentGame].remainingUsers[winningIndex].id;
       }
     },
     beginSpin: (state) => {
-      if (state.remainingUsers.length > 0) {
-        state.spinning = true;
+      if (state.games[state.currentGame].remainingUsers.length > 0) {
+        state.games[state.currentGame].spinning = true;
       }
     },
     endSpin: (state) => {
-      if (state.winningId !== null) {
-        const user = state.remainingUsers.find((u) => u.id === state.winningId);
+      if (state.games[state.currentGame].winningId !== null) {
+        const user = state.games[state.currentGame].remainingUsers.find((u) => u.id === state.games[state.currentGame].winningId);
         if (user) {
-          state.winningName = user.name;
+          state.games[state.currentGame].winningName = user.name;
         }
       }
-      state.spinning = false;
+      state.games[state.currentGame].spinning = false;
     },
     reset: (state, action: PayloadAction<{ seed: number }>) => {
-      state.remainingUsers = deepCopy(state.allUsers.filter((x) => x.checked));
-      state.winningId = null;
-      state.winningName = null;
-      state.seed = action.payload.seed;
+      state.games[state.currentGame].remainingUsers = deepCopy(state.games[state.currentGame].allUsers.filter((x) => x.checked));
+      state.games[state.currentGame].winningId = null;
+      state.games[state.currentGame].winningName = null;
+      state.games[state.currentGame].seed = action.payload.seed;
     }
   }
 });
 
-export const { addUser, removeUser, setUserName, setUserTeam, toggleUser, reset, prepareSpin, beginSpin, endSpin } = rouletteSlice.actions;
+export const { prevGame, nextGame, addUser, removeUser, setUserName, setUserTeam, toggleUser, reset, prepareSpin, beginSpin, endSpin } = rouletteSlice.actions;
 
-export const selectAllUsers = (state: RootState) => state.roulette.allUsers;
-export const selectRemainingUsers = (state: RootState) => state.roulette.remainingUsers;
-export const selectSpinning = (state: RootState) => state.roulette.spinning;
-export const selectWinningId = (state: RootState) => state.roulette.winningId;
-export const selectWinningName = (state: RootState) => state.roulette.winningName;
-export const selectSeed = (state: RootState) => state.roulette.seed;
+export const selectGame = (state: RootState) => state.roulette.currentGame;
+export const selectAllUsers = (state: RootState) => state.roulette.games[state.roulette.currentGame].allUsers;
+export const selectRemainingUsers = (state: RootState) => state.roulette.games[state.roulette.currentGame].remainingUsers;
+export const selectSpinning = (state: RootState) => state.roulette.games[state.roulette.currentGame].spinning;
+export const selectWinningId = (state: RootState) => state.roulette.games[state.roulette.currentGame].winningId;
+export const selectWinningName = (state: RootState) => state.roulette.games[state.roulette.currentGame].winningName;
+export const selectSeed = (state: RootState) => state.roulette.games[state.roulette.currentGame].seed;
---
+++ src/store/roulette/state/v2/RouletteGame.ts
@@ -0,0 +1,10 @@
+import { RouletteUser } from "./RouletteUser";
+
+export interface RouletteGame {
+    allUsers: RouletteUser[];
+    remainingUsers: RouletteUser[];
+    spinning: boolean;
+    winningId: string | null;
+    winningName: string | null;
+    seed: number;
+}
