90c5224fb01425572f000e500ab7fda347ab89d5
archerax <10404171+archerax@users.noreply.github.com>
2025-08-28 13:38:29 +0100

    Fix build error

--- src/utils/mascot.ts
+++ src/utils/mascot.ts
@@ -3,5 +3,5 @@ import { default as crc24 } from "crc/crc24";
 const numberOfPokemonImagesAvailable = 1025;
 
 export function getMascot(name: string, randomSeed: number): number {
-  return (crc24(new TextEncoder().encode(name)) ^ Math.floor(randomSeed * 16777216)) % numberOfPokemonImagesAvailable;
+  return (crc24(name) ^ Math.floor(randomSeed * 16777216)) % numberOfPokemonImagesAvailable;
 }
